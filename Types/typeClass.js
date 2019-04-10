a4928b2995db42da8484e68239aa55ac1540ce81
unknown <no.trbl.2.u@gmail.com>
2019-04-10 18:14:52 -0400

    Added type class experiment / Attempt

---
+++ Types/typeClass.js
@@ -0,0 +1,36 @@
+"use strict";
+// TYPE CLASSES
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var SameName = /** @class */ (function () {
+    function SameName() {
+    }
+    SameName.prototype.eq = function (person1, person2) {
+        return person1.name === person2.name;
+    };
+    return SameName;
+}());
+var Person = /** @class */ (function (_super) {
+    __extends(Person, _super);
+    function Person(name) {
+        var _this = _super.call(this) || this;
+        _this.name = name;
+        return _this;
+    }
+    return Person;
+}(SameName));
+var tj = new Person("TJ");
+var fred = new Person("Fred");
+var tj2 = new Person("TJ");
+console.log(tj.eq(tj, fred));
---
+++ Types/typeClass.ts
@@ -0,0 +1,30 @@
+// TYPE CLASSES
+
+// TODO: Create as a static method?
+// TODO: Create in such a way that doesn't rely on classes?
+
+interface ISameName {
+  eq: (person1: IPerson, person2: IPerson) => boolean
+}
+
+interface IPerson {
+  name: string;
+}
+
+abstract class SameName implements ISameName{
+  eq(person1: IPerson, person2: IPerson): boolean {
+    return person1.name === person2.name
+  }
+}
+
+class Person extends SameName implements IPerson {
+  constructor(public name: string){
+    super()
+  }
+}
+
+let tj = new Person("TJ")
+let fred = new Person("Fred")
+let tj2 = new Person("TJ")
+
+console.log(tj.eq(tj, fred))
